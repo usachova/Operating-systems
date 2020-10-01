6e9a67d71d879913ef8516358f0830a47b59b594
usachova <ma.usachova@gmail.com>
2020-10-01 07:57:22 +0300

    cdano

--- lab1/7.sh
+++ lab1/7.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -E -I -o -h -r "\b[A-Za-z0-9.!#%&'*+-/=?^_{}|]+@[A-Za-z0-9-]+\.[A-Za-z]{2,6}\b" /etc/* | tr "\n" "," > emails.lst
+grep -E -I -o -h -r "\b[A-Za-z0-9.!#%&'*+-/=?^_{}|]+@[A-Za-z0-9-]+\.[A-Za-z]{2,6}\b" /etc/* | tr "\n" "," | sed "s/.$/\n/" > emails.lst
