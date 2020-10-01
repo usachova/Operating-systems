6edaf95c7c4d5f2c934bef3292909e8c6796b246
usachova <ma.usachova@gmail.com>
2020-10-01 06:58:38 +0300

    add 10

--- lab1/10.sh
+++ lab1/10.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-man bash | grep -i -o "\b[a-zA-Z]\{4\}\b" | tr "[:upper:]" "[:lower:]" | sort | uniq -c | sort -n -r | head -3 | awk '{print $2}'
+man bash | grep -i -o "\b[a-zA-Z]\{4,\}\b" | tr "[:upper:]" "[:lower:]" | sort | uniq -c | sort -n -r | head -3 | awk '{print $2}'
